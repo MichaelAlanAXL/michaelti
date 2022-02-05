b0aa3f2848be7d48f920c79d7f115fa00a3f1aae
Michael Alan <michael.dtna@gmail.com>
2022-02-05 18:34:56 -0300

    removendo arquivos de config

--- index.php
+++ index.php
@@ -2,21 +2,28 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Dev\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function(){
 
-    $sql = new Dev\DB\Sql();
+    $page = new Page();
+
+    $page->setTpl("index");
+
+
+    /*$sql = new Dev\DB\Sql();
 
     $results = $sql->select("SELECT * FROM tb_usuarios");
 
     echo json_encode($results);
-    //$page = new Page;
+    //$page = new Page;*/
 
     //$page->setTpl("index");
-
 });
 
 $app->get('/hello/:name', function ($name) {
