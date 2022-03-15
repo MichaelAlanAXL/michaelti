eb3ef4728163461491a8a632eb3a511505a16f1a
Michael Alan <michael.dtna@gmail.com>
2022-03-15 16:13:33 -0300

    Add Categorias

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Dev\Page;
 use \Dev\PageAdmin;
 use \Dev\Model\User;
+use \Dev\Model\Category;
 
 $app = new Slim();
 
@@ -219,6 +220,92 @@ $app->post("/admin/forgot/reset", function(){
 
     $page->setTpl("forgot-reset-success");
 
+});
+///////////////////////////////////////////////////////////////////Categorias
+$app->get("/admin/categories", function(){
+
+    User::verifyLogin();
+
+    $categories = Category::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories", [
+        'categories'=>$categories
+    ]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+    User::verifyLogin();
+    
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-create");
+
+});
+
+
+$app->post("/admin/categories/create", function(){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header("Location: /admin/categories");
+    exit;
+    
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->delete();
+
+    header("location: /admin/categories");
+    exit;
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-update", [
+        'category'=>$category->getValues()
+    ]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header("Location: /admin/categories");
+    exit;
 });
 
 $app->run();
